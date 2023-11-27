4c321f403a5ba3b179ab582aa632bc957d2e9494
Oleksandr Levchuk <73852716+alxlvgit@users.noreply.github.com>
2023-11-26 21:13:16 -0800

    clean up

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -13,7 +13,7 @@ import { redirect } from "next/navigation";
 const s3Client = new S3Client({
   region: process.env.BUCKET_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
